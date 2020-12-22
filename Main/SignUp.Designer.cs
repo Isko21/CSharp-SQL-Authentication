4df54e8418daca75a19f61d6b2e1555fd467f0c0
Isko21 <76448222+Isko21@users.noreply.github.com>
2020-12-22 12:51:06 +0100

    Add files via upload
    Sign In & Sign Up

---
+++ Main/SignUp.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace Main
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignUp));
+            this.txtEmail = new System.Windows.Forms.Label();
+            this.txtEmailBox = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.Label();
+            this.txtConfirmPasswordBox = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.Label();
+            this.txtPasswordBox = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.Label();
+            this.txtUsernameBox = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.Label();
+            this.txtAddressBox = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.Label();
+            this.txtNumberBox = new System.Windows.Forms.TextBox();
+            this.txtSurnameBox = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.Label();
+            this.txtNameBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.AutoSize = true;
+            this.txtEmail.BackColor = System.Drawing.Color.Transparent;
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(93, 153);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(50, 23);
+            this.txtEmail.TabIndex = 39;
+            this.txtEmail.Text = "Email";
+            // 
+            // txtEmailBox
+            // 
+            this.txtEmailBox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmailBox.Location = new System.Drawing.Point(169, 153);
+            this.txtEmailBox.Name = "txtEmailBox";
+            this.txtEmailBox.Size = new System.Drawing.Size(181, 28);
+            this.txtEmailBox.TabIndex = 38;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSubmit.Location = new System.Drawing.Point(169, 405);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(62, 28);
+            this.btnSubmit.TabIndex = 37;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Font = new System.Drawing.Font("MV Boli", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(106, 20);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(244, 41);
+            this.label11.TabIndex = 36;
+            this.label11.Text = "Sign Up Please!";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Location = new System.Drawing.Point(90, 275);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(271, 13);
+            this.label10.TabIndex = 35;
+            this.label10.Text = "____________________________________________";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.AutoSize = true;
+            this.txtSurname.BackColor = System.Drawing.Color.Transparent;
+            this.txtSurname.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtSurname.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSurname.Location = new System.Drawing.Point(57, 119);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(86, 23);
+            this.txtSurname.TabIndex = 34;
+            this.txtSurname.Text = "Last Name";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.AutoSize = true;
+            this.txtConfirmPassword.BackColor = System.Drawing.Color.Transparent;
+            this.txtConfirmPassword.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtConfirmPassword.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPassword.Location = new System.Drawing.Point(3, 371);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(140, 23);
+            this.txtConfirmPassword.TabIndex = 33;
+            this.txtConfirmPassword.Text = "Confirm Password";
+            // 
+            // txtConfirmPasswordBox
+            // 
+            this.txtConfirmPasswordBox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPasswordBox.Location = new System.Drawing.Point(169, 371);
+            this.txtConfirmPasswordBox.Name = "txtConfirmPasswordBox";
+            this.txtConfirmPasswordBox.PasswordChar = '.';
+            this.txtConfirmPasswordBox.Size = new System.Drawing.Size(181, 28);
+            this.txtConfirmPasswordBox.TabIndex = 32;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.AutoSize = true;
+            this.txtPassword.BackColor = System.Drawing.Color.Transparent;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(64, 337);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(79, 23);
+            this.txtPassword.TabIndex = 31;
+            this.txtPassword.Text = "Password";
+            // 
+            // txtPasswordBox
+            // 
+            this.txtPasswordBox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPasswordBox.Location = new System.Drawing.Point(169, 337);
+            this.txtPasswordBox.Name = "txtPasswordBox";
+            this.txtPasswordBox.PasswordChar = '.';
+            this.txtPasswordBox.Size = new System.Drawing.Size(181, 28);
+            this.txtPasswordBox.TabIndex = 30;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.AutoSize = true;
+            this.txtUsername.BackColor = System.Drawing.Color.Transparent;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(60, 303);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(83, 23);
+            this.txtUsername.TabIndex = 29;
+            this.txtUsername.Text = "Username";
+            // 
+            // txtUsernameBox
+            // 
+            this.txtUsernameBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtUsernameBox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsernameBox.Location = new System.Drawing.Point(169, 303);
+            this.txtUsernameBox.Name = "txtUsernameBox";
+            this.txtUsernameBox.Size = new System.Drawing.Size(181, 28);
+            this.txtUsernameBox.TabIndex = 28;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.AutoSize = true;
+            this.txtAddress.BackColor = System.Drawing.Color.Transparent;
+            this.txtAddress.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(75, 227);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(68, 23);
+            this.txtAddress.TabIndex = 27;
+            this.txtAddress.Text = "Address";
+            // 
+            // txtAddressBox
+            // 
+            this.txtAddressBox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddressBox.Location = new System.Drawing.Point(169, 220);
+            this.txtAddressBox.Multiline = true;
+            this.txtAddressBox.Name = "txtAddressBox";
+            this.txtAddressBox.Size = new System.Drawing.Size(181, 52);
+            this.txtAddressBox.TabIndex = 26;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.AutoSize = true;
+            this.txtNumber.BackColor = System.Drawing.Color.Transparent;
+            this.txtNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtNumber.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber.Location = new System.Drawing.Point(73, 189);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(70, 23);
+            this.txtNumber.TabIndex = 25;
+            this.txtNumber.Text = "Number";
+            // 
+            // txtNumberBox
+            // 
+            this.txtNumberBox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumberBox.Location = new System.Drawing.Point(169, 186);
+            this.txtNumberBox.Name = "txtNumberBox";
+            this.txtNumberBox.Size = new System.Drawing.Size(181, 28);
+            this.txtNumberBox.TabIndex = 24;
+            // 
+            // txtSurnameBox
+            // 
+            this.txtSurnameBox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSurnameBox.Location = new System.Drawing.Point(169, 119);
+            this.txtSurnameBox.Name = "txtSurnameBox";
+            this.txtSurnameBox.Size = new System.Drawing.Size(181, 28);
+            this.txtSurnameBox.TabIndex = 23;
+            // 
+            // txtName
+            // 
+            this.txtName.AutoSize = true;
+            this.txtName.BackColor = System.Drawing.Color.Transparent;
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtName.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(55, 85);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(88, 23);
+            this.txtName.TabIndex = 22;
+            this.txtName.Text = "First Name";
+            // 
+            // txtNameBox
+            // 
+            this.txtNameBox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNameBox.Location = new System.Drawing.Point(169, 85);
+            this.txtNameBox.Name = "txtNameBox";
+            this.txtNameBox.Size = new System.Drawing.Size(181, 28);
+            this.txtNameBox.TabIndex = 21;
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(527, 450);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtEmailBox);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtConfirmPasswordBox);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtPasswordBox);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtUsernameBox);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtAddressBox);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtNumberBox);
+            this.Controls.Add(this.txtSurnameBox);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtNameBox);
+            this.Name = "SignUp";
+            this.Text = "SignUp";
+            this.Load += new System.EventHandler(this.SignUp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtEmail;
+        private System.Windows.Forms.TextBox txtEmailBox;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label txtSurname;
+        private System.Windows.Forms.Label txtConfirmPassword;
+        private System.Windows.Forms.TextBox txtConfirmPasswordBox;
+        private System.Windows.Forms.Label txtPassword;
+        private System.Windows.Forms.TextBox txtPasswordBox;
+        private System.Windows.Forms.Label txtUsername;
+        private System.Windows.Forms.TextBox txtUsernameBox;
+        private System.Windows.Forms.Label txtAddress;
+        private System.Windows.Forms.TextBox txtAddressBox;
+        private System.Windows.Forms.Label txtNumber;
+        private System.Windows.Forms.TextBox txtNumberBox;
+        private System.Windows.Forms.TextBox txtSurnameBox;
+        private System.Windows.Forms.Label txtName;
+        private System.Windows.Forms.TextBox txtNameBox;
+    }
+}
